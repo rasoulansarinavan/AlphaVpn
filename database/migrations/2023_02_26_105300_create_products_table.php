a58ad469bd3e37212f4d06e24fd6981e66306a7d
LeGion <mehrdad.dadashi@gmail.com>
2023-02-28 13:31:44 +0300

    refactor products migration

--- database/migrations/2023_02_26_105300_create_products_table.php
+++ database/migrations/2023_02_26_105300_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->integer('price');
+            $table->foreignId('server_id')->constrained();
             $table->timestamps();
         });
     }
