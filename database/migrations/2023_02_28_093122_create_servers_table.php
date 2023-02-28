f001b8dc03e75fc847826b9ec0aa1cbd4cdc72ef
LeGion <mehrdad.dadashi@gmail.com>
2023-02-28 13:29:03 +0300

    create server migration

--- database/migrations/2023_02_28_093122_create_servers_table.php
+++ database/migrations/2023_02_28_093122_create_servers_table.php
@@ -10,10 +10,11 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('servers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('price');
+            $table->string('password');
+            $table->string('ip');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('servers');
     }
 };
