1a923e8f4a6483a94c366912dd89811c81cf0d67
rasoul <rasoulansarinavan98@gmail.com>
2023-03-02 17:36:43 +0330

    add wallet view in admin panel

--- app/Http/Livewire/Admin/Wallet/Index.php
+++ app/Http/Livewire/Admin/Wallet/Index.php
@@ -2,12 +2,83 @@
 
 namespace App\Http\Livewire\Admin\Wallet;
 
+use App\Models\User;
+use App\Models\UserWallet;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 
 class Index extends Component
+
 {
+    public $user_id = '', $address_wallet = '', $wallet_id;
+    protected $listeners = ['delete', 'saveUserWallet'];
+
+    public function saveUserWallet($formData, UserWallet $wallets)
+    {
+
+
+        if ($this->wallet_id != null) {
+            $wallet_id = $this->wallet_id;
+            $validator = Validator::make($formData, [
+                'user_id' => 'required | regex:/^[ا-یa-zA-Z0-9@$#^%&*!]+$/u',
+                'address_wallet' => 'required | regex:/^[ا-یa-zA-Z0-9@$#^%&*!]+$/u',
+            ]);
+        } else {
+            $wallet_id = 0;
+            $validator = Validator::make($formData, [
+                'user_id' => 'required | regex:/^[ا-یa-zA-Z0-9@$#^%&*!]+$/u',
+                'address_wallet' => 'required | regex:/^[ا-یa-zA-Z0-9@$#^%&*!]+$/u',
+            ]);
+        }
+
+        $validator->validate();
+        $this->resetValidation();
+        $wallets->saveUserWallet($formData, $wallet_id);
+
+        $this->dispatchBrowserEvent('success', [
+            'message' => trans('alerts.success')
+        ]);
+
+        $this->user_id = '';
+        $this->address_wallet = '';
+        $this->wallet_id = '';
+
+    }
+
+
+    public function editWallet($wallet_id)
+    {
+        $wallet = UserWallet::query()->where('id', $wallet_id)->first();
+
+        $this->user_id = $wallet->user_id;
+        $this->address_wallet = $wallet->address_wallet;
+        $this->wallet_id = $wallet->id;
+    }
+
+    public function deleteConfirm($id)
+    {
+
+        $this->dispatchBrowserEvent('swal:confirm', [
+            'type' => 'warning',
+            'title' => trans('Are you sure???'),
+            'text' => '',
+            'id' => $id
+        ]);
+    }
+
+    public function delete($wallet_id)
+    {
+        UserWallet::query()->where('id', $wallet_id)->delete();
+
+        $this->dispatchBrowserEvent('success', [
+            'message' => 'The operation was successful'
+        ]);
+    }
+
     public function render()
     {
-        return view('admin.livewire.wallet.index')->extends('admin.layouts.app');
+        $wallets = UserWallet::all();
+        $users = User::all();
+        return view('admin.livewire.wallet.index', ['wallets' => $wallets, 'users' => $users])->extends('admin.layouts.app');
     }
 }
--- resources/views/admin/livewire/wallet/index.blade.php
+++ resources/views/admin/livewire/wallet/index.blade.php
@@ -1,98 +1,97 @@
-<div class="widget-content widget-content-area br-8">
-    <table id="invoice-list" class="table dt-table-hover" style="width:100%">
-        <thead>
-        <tr>
-            <th class="checkbox-column"> Record no. </th>
-            <th>Invoice Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Status</th>
-            <th>Amount</th>
-            <th>Date</th>
-            <th>Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td class="checkbox-column"> 1 </td>
-            <td><a href="./app-invoice-preview.html"><span class="inv-number">#098424</span></a></td>
-            <td>
-                <div class="d-flex">
-                    <div class="usr-img-frame me-2 rounded-circle">
-                        <img alt="avatar" class="img-fluid rounded-circle" src="/admin/src/assets/img/profile-12.jpeg">
-                    </div>
-                    <p class="align-self-center mb-0 user-name"> Alma Clarke </p>
+<section class="content-main">
+    <div class="content-header">
+        <div>
+            <h2 class="content-title card-title">Feature</h2>
+        </div>
+        <div>
+            <input class="form-control bg-white" type="text" placeholder="Search Categories">
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-3">
+                    <form wire:submit.prevent="saveUserWallet(Object.fromEntries(new FormData($event.target)))">
+                        <div class="mb-4">
+                            <label class="form-label" for="user_id">Choose USer</label>
+                            <select class="form-select  @error('user_id') error-input-border @enderror"
+                                    name="user_id" id="user_id" wire:model="user_id">
+                                <option></option>
+                                @foreach($users as $user)
+                                    <option
+                                        @if($user_id==$user->id)
+                                            selected="selected"
+                                        @endif
+                                        value="{{$user->id}}">{{$user->name}}</option>
+                                @endforeach
+                            </select>
+                            @foreach($errors->get('user_id') as $message)
+                                <sapan wire:loading.remove
+                                       class="text-danger w-100 d-block mt-2">{{$message}}</sapan>
+                            @endforeach
+                        </div>
+
+                        <div class="mb-4">
+                            <label class="form-label" for="address_wallet">Address Wallet</label>
+                            <textarea wire:model="address_wallet" name="address_wallet" id="address_wallet" class="form-control"
+                                      placeholder="Type here">{{$address_wallet}}</textarea>
+                            @foreach ($errors->get('address_wallet') as $message)
+                                <span wire:loading.remove
+                                      class=" text-danger w-100 d-block mt-2">{{ $message}}</span>
+                            @endforeach
+                        </div>
+
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-primary">Create</button>
+                        </div>
+                    </form>
                 </div>
-            </td>
-            <td><span class="inv-email"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg> alma.clarke@gmail.com</span></td>
-            <td><span class="badge badge-light-success inv-status">Paid</span></td>
-            <td><span class="inv-amount">$234.40</span></td>
-            <td><span class="inv-date"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg> 10 Feb </span></td>
-            <td>
-                <a class="badge badge-light-primary text-start me-2 action-edit" href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a>
-                <a class="badge badge-light-danger text-start action-delete" href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
-            </td>
-        </tr>
-        <tr>
-            <td class="checkbox-column"> 1 </td>
-            <td><a href="./app-invoice-preview.html"><span class="inv-number">#086643</span></a></td>
-            <td>
-                <div class="d-flex">
-                    <div class="usr-img-frame me-2 rounded-circle">
-                        <img alt="avatar" class="img-fluid rounded-circle" src="/admin/src/assets/img/profile-11.jpeg">
+                <div class="col-md-9">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th class="text-center">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="">
+                                    </div>
+                                </th>
+                                <th>#</th>
+                                <th>User Name</th>
+                                <th>Address Wallet</th>
+                                <th class="text-end">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($wallets as $wallet)
+                                <tr>
+                                    <td class="text-center">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="">
+                                        </div>
+                                    </td>
+                                    <td>{{$loop->index+1}}</td>
+                                    <td><b>{{optional($wallet->parent)->name}}</b></td>
+                                    <td>{{$wallet->address_wallet}}</td>
+                                    <td class="text-end">
+                                        <div class="dropdown"><a class="btn btn-light rounded btn-sm font-sm" href="#"
+                                                                 data-bs-toggle="dropdown"><i
+                                                    class="material-icons md-more_horiz"></i></a>
+                                            <div class="dropdown-menu">
+                                                <a class="dropdown-item" wire:click="editWallet('{{$wallet->id}}')">Edit
+                                                    info</a>
+                                                <a class="dropdown-item text-danger"
+                                                   wire:click="deleteConfirm({{$wallet->id}})">Delete</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
-                    <p class="align-self-center mb-0 user-name"> Amy Diaz </p>
                 </div>
-            </td>
-            <td><span class="inv-email"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg> amy968@gmail.com</span></td>
-            <td><span class="badge badge-light-success inv-status">Paid</span></td>
-            <td><span class="inv-amount">$100.00</span></td>
-            <td><span class="inv-date"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg> 14 Jan </span></td>
-            <td>
-                <a class="badge badge-light-primary text-start me-2 action-edit" href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a>
-                <a class="badge badge-light-danger text-start action-delete" href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
-            </td>
-        </tr>
-        <tr>
-            <td class="checkbox-column"> 1 </td>
-            <td><a href="./app-invoice-preview.html"><span class="inv-number">#098425</span></a></td>
-            <td>
-                <div class="d-flex">
-                    <div class="usr-img-frame me-2 rounded-circle">
-                        <img alt="avatar" class="img-fluid rounded-circle" src="/admin/src/assets/img/profile-4.jpeg">
-                    </div>
-                    <p class="align-self-center mb-0 user-name"> Felicia Terry </p>
-                </div>
-            </td>
-            <td><span class="inv-email"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg> feliciaterry@gmail.com</span></td>
-            <td><span class="badge badge-light-success inv-status">Paid</span></td>
-            <td><span class="inv-amount">$234.40</span></td>
-            <td><span class="inv-date"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg> 10 Feb </span></td>
-            <td>
-                <a class="badge badge-light-primary text-start me-2 action-edit" href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a>
-                <a class="badge badge-light-danger text-start action-delete" href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
-            </td>
-        </tr>
-        <tr>
-            <td class="checkbox-column"> 1 </td>
-            <td><a href="./app-invoice-preview.html"><span class="inv-number">#098445</span></a></td>
-            <td>
-                <div class="d-flex">
-                    <div class="usr-img-frame me-2 rounded-circle">
-                        <img alt="avatar" class="img-fluid rounded-circle" src="/admin/src/assets/img/profile-9.jpeg">
-                    </div>
-                    <p class="align-self-center mb-0 user-name"> Carol Mack </p>
-                </div>
-            </td>
-            <td><span class="inv-email"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg> carolmack@hotmail.com</span></td>
-            <td><span class="badge badge-light-warning inv-status">Pending</span></td>
-            <td><span class="inv-amount">$49.00</span></td>
-            <td><span class="inv-date"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg> 06 Feb </span></td>
-            <td>
-                <a class="badge badge-light-primary text-start me-2 action-edit" href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a>
-                <a class="badge badge-light-danger text-start action-delete" href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg></a>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-</div>
+            </div>
+        </div>
+    </div>
+</section>
