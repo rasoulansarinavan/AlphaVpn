2f909c1e7fdae6c20ff884656154449109043663
LeGion <mehrdad.dadashi@gmail.com>
2023-03-06 15:28:23 +0300

    refactor side-bar.blade.php client

--- resources/views/client/layouts/side-bar.blade.php
+++ resources/views/client/layouts/side-bar.blade.php
@@ -1,8 +1,10 @@
 <!--  BEGIN SIDEBAR  -->
+@php
+    $prefix=\Illuminate\Support\Facades\Route::current()->getPrefix();
+    $rout_name=\Illuminate\Support\Facades\Route::current()->getName();
+@endphp
 <div class="sidebar-wrapper sidebar-theme">
-
     <nav id="sidebar">
-
         <div class="navbar-nav theme-brand flex-row  text-center">
             <div class="nav-logo">
                 <div class="nav-item theme-logo">
@@ -40,7 +42,7 @@
         <div class="shadow-bottom"></div>
         <ul class="list-unstyled menu-categories" id="accordionExample">
 
-            <li class="menu">
+            <li class="menu {{$rout_name=='client.profile.dashboard' ? 'active' : ''}}">
                 <a href="{{route('client.profile.dashboard')}}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
@@ -48,7 +50,7 @@
                     </div>
                 </a>
             </li>
-            <li class="menu">
+            <li class="menu {{$rout_name=='client.profile.account-setting' ? 'active' : ''}}">
                 <a href="{{route('client.profile.account-setting')}}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -62,7 +64,7 @@
                     </div>
                 </a>
             </li>
-            <li class="menu">
+            <li class="menu {{$rout_name=='client.profile.pricing-table' ? 'active' : ''}}">
                 <a href="{{route('client.profile.pricing-table')}}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -76,7 +78,7 @@
                     </div>
                 </a>
             </li>
-            <li class="menu">
+  {{--          <li class="menu {{$rout_name=='client.profile.pricing-table' ? 'active' : ''}}">
                 <a href="{{route('client.profile.pricing-table')}}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -89,8 +91,8 @@
                         <span>VPN</span>
                     </div>
                 </a>
-            </li>
-            <li class="menu">
+            </li>--}}
+            <li class="menu {{$rout_name=='client.profile.wallet-index' ? 'active' : ''}}">
                 <a href="{{route('client.profile.wallet-index')}}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
