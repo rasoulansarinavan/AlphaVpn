c79d0f2078d5777f518d6ff3ecd67c60a0ed4d98
LeGion <mehrdad.dadashi@gmail.com>
2023-03-12 11:45:38 +0300

    added load into side-bar.blade.php

--- resources/views/client/layouts/side-bar.blade.php
+++ resources/views/client/layouts/side-bar.blade.php
@@ -5,26 +5,12 @@
 @endphp
 <div class="sidebar-wrapper sidebar-theme">
     <nav id="sidebar">
-        <div class="navbar-nav theme-brand flex-row  text-center">
-            <div class="nav-logo">
-                <div class="nav-item theme-logo">
-                    <a href="./index.html">
-                        <img src="/admin/src/assets/img/logo.svg" class="navbar-logo" alt="logo">
-                    </a>
-                </div>
-                <div class="nav-item theme-text">
-                    <a href="./index.html" class="nav-link"> </a>
-                </div>
-            </div>
-            <div class="nav-item sidebar-toggle">
-                <div class="btn-toggle sidebarCollapse">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                         class="feather feather-chevrons-left">
-                        <polyline points="11 17 6 12 11 7"></polyline>
-                        <polyline points="18 17 13 12 18 7"></polyline>
-                    </svg>
-                </div>
+        <div class="navbar-nav theme-brand flex-row  justify-content-center">
+
+            <div class="nav-item sidebar-toggle p-1">
+
+                    <img src="/admin/assets/img/logo.png" width="100" alt="">
+
             </div>
         </div>
         <div class="profile-info">
