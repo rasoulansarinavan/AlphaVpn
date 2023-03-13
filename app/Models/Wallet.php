c0d956523ef4d4daebb87a1954ddd7088292c1db
LeGion <mehrdad.dadashi@gmail.com>
2023-03-13 13:00:07 +0300

    made faqs and faqs category model and table

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Dflydev\DotAccessData\Data;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
---
+++ database/migrations/2023_03_14_095255_create_faqs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('faqs', function (Blueprint $table) {
+            $table->id();
+            $table->string('question_en');
+            $table->string('question_fa');
+            $table->text('answer_en');
+            $table->text('answer_fa');
+            $table->foreignId('faq_category_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('faqs');
+    }
+};
