f29ec2ad3904f58a7003e65220ce4a98fb5eba99
LeGion <mehrdad.dadashi@gmail.com>
2023-03-13 14:40:40 +0300

    added localization

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('language/{locale}', function ($locale) {
+    app()->setLocale($locale);
+    session()->put('locale', $locale);
+    return redirect()->back();
+})->name('lang');
+
 Route::get('/logout', [Register::class, 'clientLogout'])->name('client.logout')->middleware('auth:web');
 Route::get('/', Register::class)->name('register');
 
